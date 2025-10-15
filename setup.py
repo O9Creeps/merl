520b8b42b141a0b93795ff261cc2010d6d2d8751
🏳‍🌈 O9Creeps 🐯 <zslaba8949@inst.hcpss.org>
2025-10-15 13:32:57 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='merl',
+    version='0.1.0',    
+    description='A useless python lybrary',
+    url='https://github.com/O9Creeps/merl',
+    author='O9CreeperBoi',
+    author_email='not provided',    
+    license='none, do whatever you want',
+    packages=['merl'],
+    install_requires=['time',
+                      'random',
+                      ],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: Freeware',  
+        'Operating System :: OS Independent',        
+        'Programming Language :: Python :: 3'
+    ],
+)    
