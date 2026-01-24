7e76c695507c647f151a8dcf382bdff94ff9205d
🏳‍🌈 O9Creeps 🐯 <zslaba8949@inst.hcpss.org>
2026-01-24 17:24:17 -0500

    Add Markov Chain responses for Peanut Butter

--- merl/merlkov.py
+++ merl/merlkov.py
@@ -1,10 +1,17 @@
+# Merl isn't the most intelligent AI, so to mimic that,
+# I will fill them with a bit more... personality! Meet
+# my Markov Chain Engine. Cool, isn't it?
+# Of course, this only adds a *little* variation to
+# Merl's responses, and even then some chains aren't
+# perfect (looking at you, 'msg_pb'!).
+
 import random
+
 """
 reply = {
   "cap":["I'm glad you asked! I can", " do basic math with numbers ", "up to fifty seven now. Go on, give ", "me a problem!"],
   "copy":["I'm sorry, but I am designed to be a ", "guide for 'Minecraft', and not to be ", "copied. Can I help you with anything ", "else?"],
   "update":["If you are wishing to know the next ", "update, prerelease, or preview, then ", "sorry. I cannot provide that information ", "yet. Can I help you with something else?"],
-  "pb":["Are you talking about my cat, Peanut ", "Butter? If so, then bad news. They ", "died a while ago. :_("],
   "iCanHelp":["I can help you with questions related ", "to Minecraft! What do you need ", "assistance with?"],
   "greet":["Hello there! I am Merl, a support AI ", "made by Mojang. How can I help you ", "today on the topic of Minecraft?"],
   "idk2":["I don't know. Can I help you with a ", "question related to Minecraft?"]
@@ -12,8 +19,31 @@ reply = {
 """
 msg_blank = {
   "_init_" : [["1", "2"], [1, 1]],
-  "item" : [["blank"],[1]],
   " S" : [[" o", " b"],[1, 1]],
+  "a" : [["blank"], [1]]
+}
+
+msg_pb = {
+  "_init_" : [["Are you", "If you", "Peanut"], [1, 1, 1]],
+  "Are you" : [[" asking", " wanting to"],[1, 1]],
+  " asking" : [[" about my", " for info on"],[1, 1]],
+  " for info on" : [[" Peanut Butter", " my"],[1, 1]],
+  " about my" : [[" cat", " pet"],[1, 1]],
+  " wanting to" : [[" know more about my", " eat pizza?"],[100, 1]],
+  "If you" : [["'re asking about", " want to know more about"],[1, 1]],
+  "'re asking about" : [[" my", " Peanut Butter"],[1, 1]],
+  " want to know more about" : [[" my", " Peanut Butter"],[1, 1]],
+  " know more about my" : [[" cat", " pet"],[1, 1]],
+  " my" : [[" cat", " pet"],[1, 1]],
+  " pet" : [[" cat"],[1]],
+  " cat" : [[" Peanut Butter"],[1]],
+  " Peanut Butter" : [[", then sorry. ", ", because "],[1, 1]],
+  ", then sorry. " : [["Peanut"],[1]],
+  ", because " : [["Peanut", "%j"],[5, 1]],
+  "%j" : [["Peanut", "%j"],[1, 8]],
+  "Peanut" : [[" Butter is", " Butter died."],[1, 1]],
+  " Butter is" : [[" no longer", " dead.", " is also an AI, like me!"],[3, 3, 1]],
+  " no longer" : [[" with us.", " with me.", " alive.", " allowed here. Mojang is kinda cruel."],[6, 6, 6, 1]],
   "a" : [["blank"], [1]]
 }
 
@@ -25,7 +55,7 @@ msg_test_jackenstien = {
   "YOU'RE" : [[" TAKING", " TOO", " LONG"], [3, 1, 1]]
 }
 
-msg_minecrap = {
+msg_movie = {
   "_init_": [["No.", "Sorry", "Nuh uh,"], [4, 2, 1]],
   "No." : [[" No"],[1]],
   " No" : [[" no", " no.", " no!"],[3, 1, 1]],
@@ -57,4 +87,4 @@ def retmark(d: dict):
 
 
 for x in range(10):
-  print(retmark(msg_minecrap))
+  print(retmark(msg_pb))
